c1b84112bc91ff913dde518136e913bb5836f5e0
Senie Calalang <senie.c88@gmail.com>
2024-10-27 23:24:08 +0900

    Add API routes for livehouses and genres

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  #
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :livehouses, only: [:index, :create]
+      resources :genres, only: [:index]
+    end
+  end
 end
