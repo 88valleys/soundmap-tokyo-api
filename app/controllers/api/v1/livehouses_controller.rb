c7c4912de332c350d7a348990617bbaa33f38ffc
Senie Calalang <senie.c88@gmail.com>
2024-10-27 23:25:11 +0900

    Add API routes and controller for livehouses

---
+++ app/controllers/api/v1/livehouses_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::LivehousesController < ApplicationController
+  def index
+    if params[:title].present?
+      @livehouses = Livehouse.where("title ILIKE ?", "%#{params[:title]}%")
+    else
+      @livehouses = Livehouse.all
+    end
+    # Putting the most recently created livehouses first
+    render json: @livehouses.order(created_at: :desc)
+  end
+
+  def create
+    @livehouse = Livehouse.new(livehouse_params)
+    if @livehouse.save
+      render json: @livehouse, status: :created
+    else
+      render json: { error: @livehouse.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def livehouse_params
+    params.require(:livehouse).permit(:title, :address, :picture, :capacity, :website_url, :nearest_station, :event_calendar_url, hours: {}, genres: [])
+  end
+end
